4645f43ab248a7b851b7b82c2fa90b93621f215c
aymene01 <aymene01@hotmail.fr>
2024-05-30 18:26:09 +0200

    init ssh server

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gliderlabs/ssh"
+	gossh "golang.org/x/crypto/ssh"
+)
 
 func main() {
-	fmt.Println("Hello from webhooker")
+	sshPort := ":2222"
+	server := &ssh.Server{
+		Addr:    sshPort,
+		Handler: handleSSHSession,
+		ServerConfigCallback: func(ctx ssh.Context) *gossh.ServerConfig {
+			cfg := &gossh.ServerConfig{
+				ServerVersion: "SSH-2.0-sendit",
+			}
+
+			cfg.Ciphers = []string{"aes128-gcm@openssh.com"}
+
+			return cfg
+		},
+		PublicKeyHandler: func(ctx ssh.Context, key ssh.PublicKey) bool {
+			return true
+		},
+	}
+
+	log.Fatal(server.ListenAndServe())
 }
+
+func handleSSHSession(session ssh.Session) {}
